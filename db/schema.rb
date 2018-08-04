700d4d67864ab942b5dd913f43b6e47d53d0171c
thekernel <salut.amos@gmail.com>
2018-08-04 17:27:14 +0000

    #add permission feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180804170937) do
+ActiveRecord::Schema.define(version: 20180804172616) do
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20180804170937) do
     t.string "city"
     t.string "address"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "permissions", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
