25d8f6177693d5d13c7d0ca377104945a1e8f93d
thekernel <salut.amos@gmail.com>
2018-08-31 11:23:22 +0000

    fix api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,23 +2,22 @@ Rails.application.routes.draw do
 
   resources :permissions
   resources :customers
-  #resources :users
+	#resources :users
+	
+	namespace :api do
+		namespace :v1 do
+				resources :customers
+			end
+		end
+
+  
 
   #scope subdomain: 'api' do
-	  #resources :sms
 		#scope module: 'api' do
 			#namespace :v1 do
-	  			#resources :messages
+	  			#resources :customers
 	  		#end
 	  	#end
   #end
-
-  scope subdomain: 'api' do
-		scope module: 'api' do
-			namespace :v1 do
-	  			resources :customers
-	  		end
-	  	end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
